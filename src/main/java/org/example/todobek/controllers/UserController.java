4d9de24439302bc18a15ffff6a3a437acc281594
coolfire07 <kseniaskalozub6@gmail.com>
2025-02-27 12:37:30 +0300

    del cross origin path

--- src/main/java/org/example/todobek/controllers/UserController.java
+++ src/main/java/org/example/todobek/controllers/UserController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserRepository userRepository;
